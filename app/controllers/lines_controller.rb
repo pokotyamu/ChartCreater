5b26ce1f763bbad9398e3f37aeac86c1d7e46397
pokotyamu <pokotyamu@gmail.com>
2015-02-12 18:55:24 +0900

    Create line-chart to json data

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -3,11 +3,20 @@ class LinesController < ApplicationController
   def index
     category = [1,3,5,7]
     current_quantity = [1000,5000,3000,8000]
-
     @graph = LazyHighCharts::HighChart.new('graph') do |f|
-            f.title(text: 'ItemXXXの在庫の推移')
-            f.xAxis(categories: category)
-            f.series(name: '在庫数', data: current_quantity)
+      f.title(text: 'ItemXXXの在庫の推移')
+      f.xAxis(categories: category)
+      f.series(name: '在庫数', data: current_quantity)
+    end
+  end
+
+  def show
+    category = [1,3,5,7]
+    current_quantity = params[:series].split("\\n").map(&:to_i)
+    @graph = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: 'ItemXXXの在庫の推移')
+      f.xAxis(categories: category)
+      f.series(name: '在庫数', data: current_quantity)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'lines/index'
+  get 'lines/show' => 'lines#show'  
 end
